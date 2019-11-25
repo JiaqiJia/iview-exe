0d87a842c006ee1ed8350027da8a461d4a5950df
JiaqiJia <jiajiaqisb@126.com>
2019-11-25 17:31:58 +0800

    启动项目，加入iView插件，完成初运行

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,9 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    //2019-11-25 引入 iView 组件后，eslint编译不过的问题尝试用以下代码解决。
+    "vue/no-parsing-error": [2, { "x-invalid-end-tag": false }]
   },
   parserOptions: {
     parser: "babel-eslint"
---
+++ src/views/Lab.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="Labrotary">
+    <Row>
+      <Col span="12">col-12</Col>
+      <Col span="12">col-12</Col>
+    </Row>
+    <br />
+    <Row>
+      <Col span="8">col-8</Col>
+      <Col span="8">col-8</Col>
+      <Col span="8">col-8</Col>
+    </Row>
+    <br />
+    <Row>
+      <Col span="6">col-6</Col>
+      <Col span="6">col-6</Col>
+      <Col span="6">col-6</Col>
+      <Col span="6">col-6</Col>
+    </Row>
+  </div>
+</template>
+<script>
+export default {
+  name: "Lab",
+  data() {
+    return {
+      msg: "测试使用iView",
+      list: [
+        { pname: "肥皂", price: 2.5 },
+        { pname: "牙刷", price: 7 },
+        { pname: "洗衣粉", price: 5.5 },
+        { pname: "剪刀", price: 25 },
+        { pname: "毛巾", price: 3 },
+        { pname: "牙膏", price: 8 }
+      ]
+    };
+  }
+};
+</script>
+<style scoped lang="less"></style>
